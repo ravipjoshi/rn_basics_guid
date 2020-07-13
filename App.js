89738bd7a8a8a2cc326f40d79771727b457dca5c
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-13 16:38:23 -0400

    Updated flex basics code

--- App.js
+++ App.js
@@ -12,8 +12,48 @@ export default function App() {
           />
           <Button title="+" />
       </View>
-      <View>
+      <View 
+        style={{
+          padding: 10, 
+          flexDirection:'column', 
+          width: '80%', 
+          height: 300, 
+          alignItems:"center", 
+          justifyContent:"space-around"
+          }}>
+        <View 
+          style={{
+            backgroundColor : 'red',
+            width: 100,
+            flex:1,
+            //height: 100,
+            justifyContent: 'center',
+            alignItems:'center'
 
+        }}>
+           <Text> 1 </Text>
+        </View>
+        <View style={{
+            backgroundColor : 'yellow',
+            //width: 100,
+            height: 100,
+            flex:1,
+            justifyContent: 'center',
+            alignItems:'center'
+
+        }}>
+            <Text> 2 </Text>
+        </View>
+        <View style={{
+            backgroundColor : 'blue',
+            width: 100,
+            height: 100,
+            justifyContent: 'center',
+            alignItems:'center'
+
+        }}>
+            <Text> 3 </Text>
+        </View>
       </View>
     </View>
   );
