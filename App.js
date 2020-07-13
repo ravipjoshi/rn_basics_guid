5c66dee1f9a17a3c3b84b40e3fe4937cbc54e46a
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-13 16:16:21 -0400

    Creating textinput and adding button beside it

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding:40}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
+          <TextInput 
+            placeholder="Enter Your Goals"
+            style={{width: '80%', padding:10}}
+          />
+          <Button title="+" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## This is the basic feature exploring project
 
 1. Exploring Styling within the project
+
+![basic-Image](./Concepts/Basic1.0.png)
