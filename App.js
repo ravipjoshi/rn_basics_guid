2263d6d6f46d502035c0ff5962d3669a794171bc
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-13 17:14:13 -0400

    Updated functions to add items of goals

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
+  const [enteredGoal,setEnteredGoal] = useState('');
+  const [goals, setGoal ] = useState([]);
+  
+  const inputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+  
+  const addGoal = () => {
+    console.log(enteredGoal);
+    setGoal(currentGoals=>[...currentGoals,enteredGoal])
+  }
+  
+  
+  
   return (
+    
     <View style={styles.screen}>
       <View style={styles.InputContainer}>
           <TextInput 
             placeholder="Enter Your Goals"
             style={styles.InputText}
+            onChangeText = {inputHandler}
+            value={enteredGoal}
           />
-          <Button title="+" />
+          <Button title="+"  onPress={addGoal}/>
       </View>
       <View>
+            {goals.map((goal)=><View  key={goal} style={styles.listItem}><Text >{goal}</Text></View>)}
       </View>
     </View>
   );
@@ -30,6 +48,14 @@ const styles = StyleSheet.create({
   },
   InputText:{
     width: '80%', padding:10
+  },
+  listItem:{
+    padding: 10,
+    margin : 10,
+    backgroundColor: '#fcc',
+    borderColor : 'black',
+    borderWidth: 1
+
   }
 
 });
